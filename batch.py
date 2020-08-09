9a16cc26f5df0c518ba7083789f00d8f40ee21ac
AbhishekVashisht <abhishekvashisht007@gmail.com>
2020-08-09 20:06:49 +0530

    Update batch.py

--- batch.py
+++ batch.py
@@ -6,4 +6,4 @@ Root_dir="./mp4"
 Root_classes=os.listdir(Root_dir)
 for vid in Root_classes:
     vid_path=os.path.join(os.path.join(Root_dir,vid))
-    call(['python', 'demo.py', '--vid_file=' + vid_path, '--output_folder=opp','--tracking_method=pose','--staf_dir=../openpose'])
+    call(['python', 'demo.py', '--vid_file=' + vid_path, '--output_folder=opp'])
