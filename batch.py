9a16c101a222d6aa246931b0c94ad49d88f86eaf
AbhishekVashisht <abhishekvashisht007@gmail.com>
2020-08-09 19:11:38 +0530

    k

--- batch.py
+++ batch.py
@@ -1,10 +1,9 @@
 import os
-#import subprocess
-from subprocess import call
+import subprocess
+from subprocess izsmport call
 all_vid_path=[]
 Root_dir="./mp4"
 Root_classes=os.listdir(Root_dir)
 for vid in Root_classes:
     vid_path=os.path.join(os.path.join(Root_dir,vid))
     call(['python', 'demo.py', '--vid_file=' + vid_path, '--output_folder=opp','--tracking_method=pose','--staf_dir=../openpose'])
-   
